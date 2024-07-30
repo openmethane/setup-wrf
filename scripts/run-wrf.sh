b3a3351920f028673a87adb17e41cefe3c8be667
Jared Lewis <jared@jared.kiwi.nz>
2024-07-30 14:26:58 +1000

    feat: Add geo_em_dir argument

--- scripts/run-wrf.sh
+++ scripts/run-wrf.sh
@@ -8,18 +8,19 @@
 
 set -Eeuo pipefail
 
-DOMAIN=${DOMAIN:-aust-test}
-RUN_DIR=${RUN_DIR:-/opt/project/data/wrf}
-CONFIG_FILE=${CONFIG_FILE:-config/config.docker.json}
-START_DATE=${START_DATE:-2022-07-22}
+export DOMAIN_NAME=${DOMAIN_NAME:-aust-test}
+export RUN_DIR=${RUN_DIR:-/opt/project/data/wrf}
+export START_DATE=${START_DATE:-2022-07-22}
+CONFIG_FILE=${CONFIG_FILE:-config/config.docker.full.json}
+
 
 # Default end date is the next day
 # Ignores the existing END_DATE variable if it is provided.
 # setup-wrf uses a different date conventions compared to the rest of the OpenMethane project
-END_DATE=$(date '+%Y-%m-%d' -d "$START_DATE+1 days")
+export END_DATE=$(date '+%Y-%m-%d' -d "$START_DATE+1 days")
 
 # Steps of interest
 python scripts/setup_for_wrf.py -c "${CONFIG_FILE}"
-/opt/project/data/runs/${DOMAIN}/main.sh
+/opt/project/data/runs/${DOMAIN_NAME}/main.sh
 
 echo "Finished"
